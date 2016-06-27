32d5cb30808fbf2356855dc07be32f562abb09e2
Janusz Chudzynski <jchudzynski@ATC-JCHUDZYNSKI.local>
2016-06-27 16:09:48 -0500

    Changed the CEUs text

--- academy-child/content-course-grid.php
+++ academy-child/content-course-grid.php
@@ -2,7 +2,7 @@
 <div class="course-preview <?php echo ThemexCourse::$data['status']; ?>-course">
 	<div class="course-image">
 		<a href="<?php the_permalink(); ?>"><?php //the_post_thumbnail('normal');
-	
+
 		//Check if there is a video associated with the course
 		$video_id= get_post_meta($post->ID, 'course_demo_video', true);
 		if($video_id!="")
@@ -11,11 +11,11 @@
 			//http://blog.teamtreehouse.com/using-jquery-asynchronously-loading-image
 			//http://blog.teamtreehouse.com/learn-asynchronous-image-loading-javascript
 			//echo do_shortcode("[video_lightbox_vimeo5 video_id=".$video_id." width=800 height=450 ]");
-			
+
 		}else{
-			the_post_thumbnail('normal');	
+			the_post_thumbnail('normal');
 		}
-		
+
 		 ?></a>
 		<?php //if(empty(ThemexCourse::$data['plans']) && ThemexCourse::$data['status']!='private') { ?>
 		<div class="course-price product-price">
@@ -23,23 +23,31 @@
 			<div class="corner-wrap">
 				<div class="corner"></div>
 				<div class="corner-background"></div>
-			</div>			
+			</div>
 		</div>
         <div class="course-credit product-credit">
-			<div class="credit-text"><?php echo get_field("number_of_credits", $post->ID); ?>CEUs</div>
+			<div class="credit-text"><?php
+				$ceus = get_field("number_of_credits", $post->ID);
+				if( floatval($ceus) > 1){
+					echo $ceus." CEUs";
+				}
+				else{
+					echo $ceus." CEU";
+				}
+			?></div>
 			<div class="corner-wrap">
 				<div class="corner"></div>
 				<div class="corner-background"></div>
-			</div>			
+			</div>
 		</div>
-        
-        
+
+
 		<?php //} ?>
 	</div>
 	<div class="course-meta">
 		<header class="course-header">
 			<h5 class="nomargin"><a href="<?php the_permalink(); ?>">
-		<?php 
+		<?php
 		 $title_string =get_the_title($post->ID);
 			$words = preg_split("/[\s,]+/", $title_string);
 			$title_string ="";
@@ -48,26 +56,26 @@
 				$max_words = count($words);
 			}
 			for($i=0; $i<$max_words;$i++)
-			{	
+			{
 				$word = $words[$i];
-	
+
 				if($i==0) {$title_string= $word;}
 				else{
-								$title_string=	$title_string." ".$word;			
+								$title_string=	$title_string." ".$word;
 				}
-	
+
 			}
 			if(count($words)>$max_words){
 				$title_string = $title_string."... ";
 			}
-			
+
 			echo $title_string; ?>
-       
-        
-        
+
+
+
       </a>
       </h5>
-		
+
 		</header>
 		<?php if(!ThemexCore::checkOption('course_popularity') || !ThemexCore::checkOption('course_rating')) { ?>
 		<footer class="course-footer clearfix">
