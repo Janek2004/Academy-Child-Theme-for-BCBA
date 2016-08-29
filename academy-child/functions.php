2ba6301d6e268fad796099b61b67556d9ffc2c03
Janusz Chudzynski <janusz@izotx.com>
2016-08-29 11:30:00 -0500

    Autocomplete order

--- academy-child/functions.php
+++ academy-child/functions.php
@@ -12,6 +12,17 @@ function register_submenu_pages() {
 	add_submenu_page( 'users.php', 'Email Blast', 'Email Blast', 'manage_options', 'EMAIL-BLAST', 'register_users_submenu_page_callback' );
 }
 
+//Autocomplete order
+add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order' );
+function custom_woocommerce_auto_complete_order( $order_id ) {
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+}
+
 
 function register_users_submenu_page_callback(){
 	?>
