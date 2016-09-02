8d4824b6407e6b3b3b4d9cb52983182c4535cc29
Janusz Chudzynski <janusz@izotx.com>
2016-09-02 14:00:48 -0500

    Added back framework files

---
+++ academy-child/framework/strings.php
@@ -0,0 +1,5 @@
+<?php 
+$strings['name-name']=__('Name', 'academy');
+$strings['email-name']=__('Email', 'academy');
+$strings['message-name']=__('Message', 'academy');
+$strings['contact-message']=__('Thank you for contacting us. We will respond shortly.', 'academy');
